e14009c206659a7455877d326e3eb462685adfa2
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-06-27 14:25:57 -0400

    refactored redux Art Gallery to include connect and middleware

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import './App.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { clearData, inputId, incrementId, decrementId, fetchData } from './features/dataSlice';
+import "./App.css";
+import { useSelector, useDispatch, connect } from "react-redux";
+import {
+  clearData,
+  inputId,
+  incrementId,
+  decrementId,
+  fetchData,
+} from "./features/dataSlice";
+import { useEffect } from "react";
 
-function App() {
-  const dispatch = useDispatch()
-  const data = useSelector((state) => state.data)
+function App(props) {
+  const dispatch = useDispatch();
+  const data = useSelector((state) => state.data);
 
   const renderImg = () => {
-    if(data.apiData) {
-      return <img style={{'width': '100vw'}} src={data.apiData.primaryImage} alt={data.apiData.title} />
+    if (data.apiData) {
+      return (
+        <img
+          style={{ width: "100vw" }}
+          src={data.apiData.primaryImage}
+          alt={data.apiData.title}
+        />
+      );
     } else {
-      return <p>image here</p>
+      return <p>image here</p>;
     }
-  }
+  };
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [props.objectId, dispatch]);
 
   return (
     <div className="App">
@@ -22,7 +39,12 @@ function App() {
         <button onClick={() => dispatch(incrementId())}>Next</button>
         <button onClick={() => dispatch(decrementId())}>Back</button>
       </div>
-      <input value={data.objectId} onChange={(e) => {dispatch(inputId(Number(e.target.value)))}} />
+      <input
+        value={data.objectId}
+        onChange={(e) => {
+          dispatch(inputId(Number(e.target.value)));
+        }}
+      />
       <div>
         {data.objectId}
         {renderImg()}
@@ -31,4 +53,8 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  objectId: state.data.objectId,
+});
+
+export default connect(mapStateToProps)(App);
